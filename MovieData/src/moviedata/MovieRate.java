3fb3fcb878d7bfbda4234e0392c6efc710aaa594
alyssoneq <alyssoneq@gmail.com>
2020-12-10 00:05:53 -0300

    Uploading the enum file.

---
+++ MovieData/src/moviedata/MovieRate.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package moviedata;
+
+/**
+ *
+ * @author Alysson Bruno
+ */
+public enum MovieRate {
+    G, PG, PG13, R, NC17
+}
