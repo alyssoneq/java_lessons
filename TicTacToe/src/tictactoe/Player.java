cdf2408a31bd53d6e066480ae96c0f76f5a9602e
alyssoneq <alyssoneq@gmail.com>
2020-12-11 12:11:03 -0300

    Implementing the framework for a registration in a ranking for every game

---
+++ TicTacToe/src/tictactoe/Player.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tictactoe;
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author Alysson Bruno
+ */
+class Player implements Serializable{
+    public String name;
+    public int victories, defeats;
+}
--- TicTacToe/src/tictactoe/TicTacToe.java
+++ TicTacToe/src/tictactoe/TicTacToe.java
@@ -19,7 +19,9 @@ public class TicTacToe {
     // Initial elements of the game 
     private static int currentPlayer, row, column, winner;
     private static int[][] spot = new int[3][3];
-    private static String player1, player2;
+    private static Player player1, player2;
+    private static Player[] players = new Player[50];
+    private static int numberOfPlayers = 0;
     private static long timer, playTime;
     
     // Instance of the Scanner
@@ -58,15 +60,22 @@ public class TicTacToe {
         playTime = getPlayTime();
         // Checking for win or draw
         if (winner == 1 ) {
-            System.out.println("\n\nThe winner is "+player1);
+            System.out.println("\n\nThe winner is "+player1.name);
+            player1.victories += 1;
+            player2.defeats += 1;
         }else if( winner == 2){
-            System.out.println("\n\nThe winner is "+player2);
+            System.out.println("\n\nThe winner is "+player2.name);
+            player2.victories += 1;
+            player1.defeats += 1;
         }else {
             System.out.println("Draw");
         }
         
         // Displaying the play time
         System.out.println("The total play time was "+playTime+"s");
+        
+        // Displaying the player ranking
+        printRanking();
         
     }
     
@@ -108,10 +117,10 @@ public class TicTacToe {
         // Determining the current player
         if (player == 1) {
             currentPlayer = 1;
-            System.out.println("\n\n Player "+player1+" turn");
+            System.out.println("\n\n Player "+player1.name+" turn");
         } else {
             currentPlayer = 2;
-            System.out.println("\n\n Player "+player2+" turn");
+            System.out.println("\n\n Player "+player2.name+" turn");
         }
         
         
@@ -192,9 +201,11 @@ public class TicTacToe {
         System.out.println("Hello!!");
         System.out.println("Please! Insert the name of each player");
         System.out.println("Player 1 name: ");
-        player1 = userInput.nextLine();
+        String playerName1 = userInput.nextLine();
+        player1 = searchPlayer(playerName1);
         System.out.println("Player 2 name: ");
-        player2 = userInput.nextLine();
+        String playerName2 = userInput.nextLine();
+        player2 = searchPlayer(playerName2);
         
         System.out.println("Thank you - Lets play!!");
     }
@@ -208,5 +219,40 @@ public class TicTacToe {
         // Returning the value of time spent in the game in seconds
         return (System.currentTimeMillis() - timer)/1000;
     }
+    
+    // This function creates and grabs names of players from the players array
+    private static Player searchPlayer(String name) {
+        Player player = null;
+        for (int i = 0; (i < numberOfPlayers) && (player == null); i++) {
+            if (players[i] != null &&
+                players[i].name.equalsIgnoreCase(name)) {
+                player = players[i];
+            }
+        }
+        
+        if (player == null) {
+            player = new Player();
+            player.name = name;
+            if(numberOfPlayers < 50){
+                players[numberOfPlayers] = player;
+                numberOfPlayers += 1;
+            }
+        }
+        
+        return player;
+    }
+    
+    // Procedure to print the players ranking
+    private static void printRanking() {
+        System.out.println("--- Players Ranking ---");
+        for (int i = 0; i < numberOfPlayers; i++) {
+            System.out.println("Name: "+players[i].name);
+            System.out.println("Victories : "+players[i].victories);
+            System.out.println("Defeats: "+players[i].defeats);
+            
+        }
+    }
+    
+    
     
 }
